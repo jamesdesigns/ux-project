64215a4faad5e7ebb0f678566003d76168f8af1e
James Hooper <jameshooperdesigns@gmail.com>
2019-12-14 23:51:15 -0700

    Add randomColors to the color of the rules

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 
 // Assign UX rules into an array
 let rules = ['Design for users.', 'Provide absolute clarity.', 'Give users control.', 'Predict, then adapt.', 'Maintain consistency.', 'Treat users as customers and vice-versa.', 'Communicate value proposition.'];
+
 // Get the ID where the rules will output
 let newRule = document.getElementById("rules");
 // Make the rules output randomly using the Math.random
 newRule.innerHTML = rules[Math.floor(Math.random() * rules.length)];
+
+let colors = ['#003399','#282828','#F0F0F0'];
+let randomColors = colors[Math.floor(Math.random()*colors.length)];
+
+let x = document.getElementById('rules');
+x.style.color = randomColors;
