f771f36c84ac1872d6651c51a211e79a9239040b
James Hooper <jameshooperdesigns@gmail.com>
2019-12-16 15:38:35 -0700

    Edit the menu of the index

--- script.js
+++ script.js
@@ -15,9 +15,8 @@ let x = document.getElementById('rules');
 x.style.color = randomColors;
 
 // GreenSock MENU
-// gsap.to(".tapBtn", {y: -10, duration: 1});
-gsap.to("#menuBtn", {y:-30, duration: 1});
-// gasp.to(".footerBox", {y: -400, duration: 1});
+gsap.to("#menuBtn", {y:-5, duration: 1});
+
 
 
 // function displayMenu() {
