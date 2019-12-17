0ae4a5e393e7d074a1d745b1c6b7f6890b01bd92
James Hooper <jameshooperdesigns@gmail.com>
2019-12-16 18:09:43 -0700

    Add extra rules to the rules array in the script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // JavaScript 
 
 // Assign UX rules into an array
-let rules = ['Design for users.', 'Provide absolute clarity.', 'Give users control.', 'Predict, then adapt.', 'Maintain consistency.', 'Treat users as customers and vice-versa.', 'Communicate value proposition.'];
+let rules = ['Design for users.', 'Always have an obvious way to access the navigation menu.', 'Design for color blind users.', 'Provide absolute clarity.', 'Give users control.', 'Predict, then adapt.', 'Maintain consistency.', 'Treat users as customers and vice-versa.', 'Communicate value proposition.'];
 
 // Get the ID where the rules will output
 let newRule = document.getElementById("rules");
